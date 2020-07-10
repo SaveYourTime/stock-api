86c5b269cd201e962885f0074a95f6570cdd7efa
Aaron Lu <aaron.lu@mvpfastlane.com>
2020-07-11 00:56:16 +0800

    Set up cron job

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ScheduleModule } from '@nestjs/schedule';
 import { TypeOrmConfig } from './config/ormconfig';
 import { StocksModule } from './stocks/stocks.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(TypeOrmConfig), StocksModule],
+  imports: [
+    TypeOrmModule.forRoot(TypeOrmConfig),
+    ScheduleModule.forRoot(),
+    StocksModule,
+  ],
   controllers: [],
   providers: [],
 })
--- src/stocks/stocks.service.ts
+++ src/stocks/stocks.service.ts
@@ -1,11 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { Cron } from '@nestjs/schedule';
 import { StockRepository } from './stock.repository';
 import { Stock } from './stock.entity';
 
 @Injectable()
 export class StocksService {
   constructor(private stockRepository: StockRepository) {}
+
   async findOne(id: number): Promise<Stock> {
     return await this.stockRepository.findOne(id);
+  }
+
+  @Cron('0 0 15 * * 1-5') // Monday to Friday at 03:00pm
+  handleCron(): void {
+    console.log('Called on Monday to Friday at 03:00pm');
   }
 }
